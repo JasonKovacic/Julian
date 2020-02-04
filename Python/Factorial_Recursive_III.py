0da71599980e38c73b6d4cbe7677e9fad820400d
Jason <kovacic.jason@gmail.com>
2020-02-03 21:20:08 -0500

    Add files via upload
    Written from scratch improved format

---
+++ Python/Factorial_Recursive_III.py
@@ -0,0 +1,26 @@
+# asks the user for a number, creates variable factorial_total
+print("This program finds the factorial of a number.\n")
+print ("Which number would you like to find?\n")
+n = input()
+
+# checks to see if the number is negative or 0 or 1
+def checker(n):
+    factorial_total = 1
+    n = int(n)
+    if n < 0:
+        print("This is a negative number.\n  Please enter a new number to find the factorial of.")
+    if n == 0: 
+        print("The factorial of 0 is 1.")
+    if n == 1: 
+        print("The factorial of 1 is 1.")
+    return fact(n)
+
+# finds the factorial of a n using a base case and recursion
+def fact(n):
+    if (n == 1):
+        return 1
+    return n * fact(n - 1)
+
+# calls the checker function and passes n through it as an argument, assigns the result to a variable and prints it out
+factorial_total = checker(n)
+print("The factorial of ", n, " is ", factorial_total, ".")
